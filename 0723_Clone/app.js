41b952e79ea8fb7c3dec7adc7211e583d0ca5336
achess <chout1213@gmail.com>
2020-07-25 15:57:47 +0900

    0725_Clone

--- 0723_Clone/app.js
+++ 0723_Clone/app.js
@@ -1,55 +1,56 @@
-document.querySelector('.main').addEventListener('submit',submitList);
-document.querySelector('ul').addEventListener('click', checkList);
+document.querySelector('.main').addEventListener('submit', handleSubmitForm);
+document.querySelector('ul').addEventListener('click', 
+handleClickDeleteOrCheck);
+document.getElementById('clearAll').addEventListener('click', handleClearAll);
 
+function handleSubmitForm(e) {
+    let input = document.querySelector('input');
+    if(input.value != ''){
+        addTodo(input.value);
+    }
+    else   
+        alert('no Input');
+    input.value = '';
+}
 
-
-// function
-
-function submitList(e) {
+function handleClickDeleteOrCheck(e) {
     
-    let content = document.querySelector('input');
-    e.preventDefault();
-    if(content.value != '')
-        addList(content.value);
-    else
-        alert('No Content');
-     
-    console.log('No Content');
-    content.value = '';
+    const name = e.target.id;
+    if(name === 'checkButton'){
+        console.log('check');
+        let item = e.target.parentNode;
+        if(item.style.textDecoration == 'line-through')
+            item.style.textDecoration = 'none';
+        else
+            item.style.textDecoration = 'line-through';
+    }
+
+    else if(name === 'deleteButton'){
+        console.log('delete');
+        const item = e.target.parentNode;
+        item.remove();
+    }
+    else {    
+        console.log(name + ': ' + 'end');
+    }
 }
 
-function checkList(e) {
-    if(e.target.name == 'checkButton')
-        checkTodo(e);
-    if(e.target.name == 'deleteButton')
-        deleteTodo(e);
+function handleClearAll(e) {
+    document.querySelector('ul').innerHTML = '';
 }
 
 
-// helper
-function addList(todo) {
+// Helpers
+function addTodo(todo) {
     let ul = document.querySelector('ul');
     let li = document.createElement('li');
 
     li.innerHTML = `
-    <span class="todo-item">${todo}</span>
-    <button name="checkButton"><i class="fas fa-check-square"></i></button>
-    <button name="deleteButton"><i class="fas fa-trash"></i></button>
+        <span class="todo-item">${todo}</span>
+        <button id="checkButton"><i class="fas fa-check-square"></i></button>
+        <button id="deleteButton"><i class="fas fa-trash"></i></button>
     `;
 
     li.classList.add('todo-item');
-    ul.appendChild(li);   
-}
-
-function checkTodo(e) {
-    let item = e.target.parentNode;
-
-    if(item.style.textDecoration == 'line-through')
-        item.style.textDecoration = 'none';
-    else   
-        item.style.textDecoration = 'line-through';
-}
-
-function deleteTodo(e) {
-
+    ul.appendChild(li);
 }
